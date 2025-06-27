117d1837245092c887a6a5a2f136a177bebc7041
kazneo <kazneo213@gmail.com>
2025-06-27 19:00:05 -0400

    updated products page

--- assets/script.js
+++ assets/script.js
@@ -14,3 +14,22 @@ if (menuButton && dropdownMenu) {
         }
     });
 }
+
+const carousel = document.getElementById('carousel');
+const images = carousel.children;
+const totalImages = images.length;
+let index = 0;
+
+document.getElementById('prev').addEventListener('click', () => {
+    index = (index - 1 + totalImages) % totalImages;
+    updateCarousel();
+});
+
+document.getElementById('next').addEventListener('click', () => {
+    index = (index + 1) % totalImages;
+    updateCarousel();
+});
+
+function updateCarousel() {
+    carousel.style.transform = `translateX(-${index * 100}%)`;
+}
